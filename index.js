0978b109df817d4ea006cec5a261ba619cde77b5
davidkov111 <kovacsdavid648@outlook.com>
2024-06-20 14:58:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { VoiceResponse } = require('twilio').twiml;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const mobileNumbers = [40770714073];
+
+app.post('/voice', (req, res) => {
+    const response = new VoiceResponse();
+    const dial = response.dial();
+
+    mobileNumbers.forEach(number => {
+        dial.number(number);
+    });
+
+    res.type('text/xml');
+    res.send(response.toString());
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
