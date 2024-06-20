1c2ef95b1873471209927509309f8e8c22cfc0d5
davidkov111 <kovacsdavid648@outlook.com>
2024-06-20 15:52:50 +0300

    commit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { VoiceResponse } = require('twilio').twiml;
-const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 
 const mobileNumbers = [40770714073];
 
@@ -25,3 +23,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// If you have installed and set up Ngrok, simply run the following 
+// command after starting the Node server to make it accessible from the internet.
+// ngrok http 3000
